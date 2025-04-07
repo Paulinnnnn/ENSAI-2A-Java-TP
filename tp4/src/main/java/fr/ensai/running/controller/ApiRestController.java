1f4198d6b4877050eddf02d3578c8ded3ca48b9a
Paulinnnnn <lemoine.paulin@gmail.com>
2025-04-07 08:44:32 +0000

    debut tp4

--- tp4/src/main/java/fr/ensai/running/controller/ApiRestController.java
+++ tp4/src/main/java/fr/ensai/running/controller/ApiRestController.java
@@ -3,7 +3,12 @@ package fr.ensai.running.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +29,21 @@ public class ApiRestController {
     public List<Athlete> allAthletes() {
 
         return athleteService.findAll();
+    }
+
+    @GetMapping("/athlete/{id}")
+    public Athlete getAthleteById(@PathVariable Long id){
+        return athleteService.findById(id);
+    }
+
+    @DeleteMapping("/athlete/{id}")
+    public void deleteAthleteById(@PathVariable Long id){
+        athleteService.deleteById(id);
+    }
+
+    @PostMapping("/athlete")
+    public void addAthleteBy(@RequestBody Athlete athlete){
+        athleteService.save(null);
     }
 
 }
